46f79babd8e68479652d0a1633ffc15e8ba4c8c2
kliffrog <kliffrog@gmail.com>
2017-11-10 00:29:34 -0600

    PostingBeta
    posting form
    generic data added

---
+++ HireMeFinal/Controllers/PostingController.cs
@@ -0,0 +1,27 @@
+﻿using HireMeFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HireMeFinal.Controllers
+{
+    public class PostingController : Controller
+    {
+        public ActionResult Posting()
+        {
+            MireMeEntities db = new MireMeEntities();
+            jobPosting posting = db.jobPostings.SingleOrDefault(x => x.companyID == 3);
+
+            PostingViewModel postingVM = new PostingViewModel();
+
+            postingVM.jobDescription = posting.jobDescription;
+            postingVM.jobTitle = posting.jobTitle;
+            postingVM.companyID = posting.companyID;
+
+            return View(postingVM);
+        }
+        
+    }
+}
---
+++ HireMeFinal/Models/PostingViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HireMeFinal.Models
+{
+    public class PostingViewModel
+    {
+        public int jobPostingID { get; set; }
+        public int jobTypeID { get; set; }
+        public int companyID { get; set; }
+        public int jobLocationID { get; set; }
+        public int userAccountID { get; set; }
+        public int jobCategoryID { get; set; }
+        public int applicantTypeID { get; set; }
+        public System.DateTime createdDate { get; set; }
+        public string jobDescription { get; set; }
+        public string jobTitle { get; set; }
+        public int numOpenings { get; set; }
+        public int hoursPerWeek { get; set; }
+        public decimal wageSalary { get; set; }
+        public System.DateTime startDate { get; set; }
+        public System.DateTime endDate { get; set; }
+        public string qualifications { get; set; }
+        public string applicationInstructions { get; set; }
+        public string applicationWebsite { get; set; }
+        public System.DateTime postingDate { get; set; }
+        public System.DateTime expirationDate { get; set; }
+        public bool enabled { get; set; }
+        public int numViews { get; set; }
+    }
+}
